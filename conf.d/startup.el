80cef0c70f2253b1da7012d20a19578b14b20c32
Timothy Klim <klimtimothy@gmail.com>
2013-10-09 13:40:07 +0400

    new init from scratch

---
+++ conf.d/startup.el
@@ -0,0 +1,11 @@
+; Disable splash screen
+(setq inhibit-splash-screen -1)
+
+; Disable backups
+(setq backup-inhibited -1)
+
+; Disable auto save
+(setq auto-save-default nil)
+
+; Show line numbers
+(global-linum-mode -1)
