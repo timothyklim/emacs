1108807aef48a800e1cb257a05ea9e29aafdeee5
Timothy Klim <klimtimothy@gmail.com>
2014-12-13 18:56:10 +0300

    fix ensime initialization

--- init.el
+++ init.el
@@ -10,6 +10,9 @@
 
 (add-to-list 'load-path (f-join user-emacs-directory "vendor"))
 
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
 (mapc (lambda (path)
   (load
    (f-join confd-directory path)))
--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ set -e
 
 cabal update
 cabal sandbox init
-cabal install --upgrade ghc-mod structured-haskell-mode hasktags stylish-haskell hlint hindent # ghci-ng
+cabal install --upgrade ghc-mod structured-haskell-mode hasktags stylish-haskell hlint hindent hdevtools # ghci-ng
 
 curl -o vendor/hindent.el https://raw.githubusercontent.com/chrisdone/hindent/master/elisp/hindent.el
